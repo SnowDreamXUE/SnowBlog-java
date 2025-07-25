ca2aadca05ddd60ac06409be8a5a31442f64b822
SnowDreamXUE <1505185702@qq.com>
2025-07-25 15:28:13 +0800

    修改友链控制器URL前缀

--- src/main/java/com/snow/blog/controller/FriendLinksController.java
+++ src/main/java/com/snow/blog/controller/FriendLinksController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/friend-links")
+@RequestMapping("/links")
 public class FriendLinksController {
 
     @Resource
